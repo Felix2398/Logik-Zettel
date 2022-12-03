2adb356da488c2c742b18fc146fc4cbf8d27663b
Felix2398 <104097884+Felix2398@users.noreply.github.com>
2022-12-03 01:40:00 +0100

    Add files via upload

---
+++ Aufgabe-3.pl
@@ -0,0 +1,14 @@
+essen :- mensa.
+essen :- restaurant.
+restaurant :- mittwoch.
+restaurant :- donnerstag.
+mensa :- vorlesung, menu.
+vorlesung :- dienstag.
+vorlesung :- donnerstag.
+menu :- kartoffeln, sauce.
+menu :- spaghetti.
+
+donnerstag.
+kartoffeln.
+spaghetti.
+
